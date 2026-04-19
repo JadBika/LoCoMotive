d92388aee3f7eef51224dda11b5d8ecc3e16f72a
Jad Bikarbass <jad.bikarbass.azmi@umontreal.ca>
2026-04-19 16:02:39 -0400

    No logging for now

--- RL/scripts/train.py
+++ RL/scripts/train.py
@@ -21,7 +21,7 @@ model = SAC(
     buffer_size=50_000,
     batch_size=256,
     learning_starts=200,
-    tensorboard_log="./tb_logs/",
+    # tensorboard_log="./tb_logs/", no tensorboard for now
     device = "cpu"
 )
 
